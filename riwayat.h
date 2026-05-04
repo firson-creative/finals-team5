cea29c2b5367bce7bcb8d7d34c2a30ca19e43711
alyafrl777 <alyafrl777@gmail.com>
2026-05-04 09:10:42 +0800

    menambahkan fitur riwayat

---
+++ riwayat.h
@@ -0,0 +1,35 @@
+#ifndef RIWAYAT_H
+#define RIWAYAT_H
+#include <string>
+using namespace std;
+
+struct Node {
+    string namaObat;    
+    string jenis;       
+    int    jumlah;      
+    string tanggal;     
+    string keterangan;  
+    Node*  next;        
+
+    Node(string namaObat, string jenis, int jumlah,
+         string tanggal, string keterangan = "")
+        : namaObat(namaObat), jenis(jenis), jumlah(jumlah),
+          tanggal(tanggal), keterangan(keterangan), next(nullptr) {}
+};
+
+class Riwayat {
+private:
+    Node* head;     
+    int   total;   
+
+public:
+    Riwayat();
+    ~Riwayat();
+    void tambah(string namaObat, string jenis, int jumlah,
+                string tanggal, string keterangan = "");
+    void tampilkan() const;
+    bool cari(const string& namaObat) const;
+    void hapusSemua();
+    int getTotalTransaksi() const;
+};
+#endif 
