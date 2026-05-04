35f763e06ae9f6f3b42b19ef5691e5428c73ff9d
bleme31 <dedewillem12@gmail.com>
2026-05-04 16:38:38 +0800

    penambahan file obat.h dan obat.cpp untuk manajemen obat

---
+++ obat.h
@@ -0,0 +1,45 @@
+#ifndef OBAT_H
+#define OBAT_H
+
+#include <string>
+#include <vector>
+#include <iostream>
+#include <iomanip>
+
+using namespace std;
+
+struct Obat {
+    string kode;
+    string nama;
+    int stok;
+    string tanggal_kadaluarsa;
+    Obat* left;
+    Obat* right;
+};
+
+class BSTObat {
+private:
+    Obat* root;
+
+    Obat* insertRekursif(Obat* node, string kode, string nama, int stok, string exp);
+    Obat* cariMin(Obat* node);
+    Obat* hapusRekursif(Obat* node, string kode, bool& terhapus);
+    Obat* cariNode(Obat* node, string kode);
+    void inorderTampil(Obat* node);
+    void inorderToArray(Obat* node, vector<Obat>& arr);
+
+public:
+    BSTObat();
+    
+    void insert(string kode, string nama, int stok, string exp);
+    bool tambahStok(string kode, int jumlah);
+    bool kurangiStok(string kode, int jumlah);
+    bool hapus(string kode);
+    void binarySearch(string kode);
+    void tampilkan();
+    string getName(string kode);
+    
+    vector<Obat> toArray();
+};
+
+#endif
